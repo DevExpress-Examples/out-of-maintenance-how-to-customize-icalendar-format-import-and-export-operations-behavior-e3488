1a909e49ebd834c3d4e16bbdeeaeaf6a5ff37120
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HatchAppointmenStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using DevExpress.XtraScheduler;
+
+namespace ScheduleriCalendarExchangeCustomization {
+    public class HatchAppointmenStatus : AppointmentStatus {
+        private Color foreColor;
+        private Color backColor;
+
+        public HatchAppointmenStatus(int id, Color foreColor, Color backColor, string caption)
+            : base((AppointmentStatusType)id, caption) {
+
+            this.foreColor = foreColor;
+            this.backColor = backColor;
+            base.UpdateBrush();
+        }
+
+        protected override Brush CreateBrush() {
+            return new HatchBrush(HatchStyle.Cross, foreColor, backColor);
+        }
+    }
+}
---
+++ VB/HatchAppointmenStatus.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports DevExpress.XtraScheduler
+
+Namespace ScheduleriCalendarExchangeCustomization
+	Public Class HatchAppointmenStatus
+		Inherits AppointmentStatus
+		Private foreColor As Color
+		Private backColor As Color
+
+		Public Sub New(ByVal id As Integer, ByVal foreColor As Color, ByVal backColor As Color, ByVal caption As String)
+			MyBase.New(CType(id, AppointmentStatusType), caption)
+
+			Me.foreColor = foreColor
+			Me.backColor = backColor
+			MyBase.UpdateBrush()
+		End Sub
+
+		Protected Overrides Function CreateBrush() As Brush
+			Return New HatchBrush(HatchStyle.Cross, foreColor, backColor)
+		End Function
+	End Class
+End Namespace
